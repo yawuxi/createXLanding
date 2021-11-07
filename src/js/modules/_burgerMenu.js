8a8457a68fd79abe2023bd4d79d69cc58004d157
yawuxi <ryliov.work@gmail.com>
2021-11-07 23:37:49 +0200

    сделал header

--- src/js/modules/_burgerMenu.js
+++ src/js/modules/_burgerMenu.js
@@ -1,12 +1,12 @@
 //burgerMenu
-const burgerBtn = document.querySelector('.burger-menu__content'),
-	burgerMenu = document.querySelector('.header__navigation'),
+const burgerBtn = document.querySelector('.burger-menu'),
+	burgerMenu = document.querySelector('.menu__list'),
 	header = document.querySelector('.header'),
 	body = document.body;
 
 burgerBtn.addEventListener('click', () => {
-	burgerBtn.classList.toggle('active');
-	burgerMenu.classList.toggle('active');
+	burgerBtn.classList.toggle('menu--active');
+	burgerMenu.classList.toggle('menu--active');
 	body.classList.toggle('lock');
 	if (document.documentElement.offsetWidth > 768) {
 		if (header.style.paddingRight == '17px' && body.style.paddingRight == '17px') {
